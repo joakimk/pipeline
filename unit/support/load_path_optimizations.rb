27073a2b67c5924b2a4509df631f16205620771f
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-10-04 22:52:40 +0200

    Tweak travis build script. Disable load path optimizations.

--- unit/support/load_path_optimizations.rb
+++ unit/support/load_path_optimizations.rb
@@ -4,9 +4,10 @@ def add_load_path(path)
   $: << full_path
 end
 
-rails_version = File.readlines(File.join(File.dirname(__FILE__), "../../Gemfile")).
-                     find { |line| line.include?("'rails'") }.split.last.gsub("'",'')
+unless ENV['TRAVIS']
+  rails_version = File.readlines(File.join(File.dirname(__FILE__), "../../Gemfile")).
+                       find { |line| line.include?("'rails'") }.split.last.gsub("'",'')
 
-add_load_path "gems/activesupport-#{rails_version}/lib"
-add_load_path "bundler/gems/rspec-roles-cb2889362a75/lib"
-
+  add_load_path "gems/activesupport-#{rails_version}/lib"
+  add_load_path "bundler/gems/rspec-roles-cb2889362a75/lib"
+end
