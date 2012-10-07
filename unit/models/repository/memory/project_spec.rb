828ac364ba6670043c8cf184649fc00df6b1dac3
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-10-08 01:02:20 +0200

    Fixing unit fail. There is no base repo role anymore.

--- unit/models/repository/memory/project_spec.rb
+++ unit/models/repository/memory/project_spec.rb
@@ -1,6 +1,5 @@
 require 'repository/memory/project'
 
 describe Repository::Memory::Project do
-  implements_role :base_repository
   implements_role :project_repository
 end
--- unit/models/repository/pg/project_spec.rb
+++ unit/models/repository/pg/project_spec.rb
@@ -1,6 +1,5 @@
 require 'repository/pg/project'
 
 describe Repository::PG::Project do
-  implements_role :base_repository
   implements_role :project_repository
 end
