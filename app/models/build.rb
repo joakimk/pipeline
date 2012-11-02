277446f062d96511babdf05b69456b62fc1312bf
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-11-02 21:59:21 +0100

    Using the new minimapper typed attributes.

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,12 +1,5 @@
 require 'minimapper/entity'
 
 class Build < Minimapper::Entity
-  attributes :project_id, :step, :revision, :status
-
-  alias_method :old_project_id=, :project_id=
-
-  # TODO: implement attribute conversions for basic types in minimapper
-  def project_id=(project_id)
-    self.old_project_id = project_id.to_i
-  end
+  attributes [ :project_id, Integer ], :step, :revision, :status
 end
--- unit/models/build_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'build'
-
-describe Build, "project_id=" do
-  it "converts string to integer" do
-    Build.new(project_id: '20').project_id.should == 20
-  end
-end
