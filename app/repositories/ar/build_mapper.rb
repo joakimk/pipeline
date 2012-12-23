c43d2a40c970724bafb3f242232d4a0537693fb7
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-12-23 22:49:24 +0100

    Update minimapper. Mass assignment of protected attributes fix.

--- app/repositories/ar/build_mapper.rb
+++ app/repositories/ar/build_mapper.rb
@@ -12,7 +12,15 @@ module AR
   end
 
   class Build < ActiveRecord::Base
-    # TODO: find out why minimapper assigns id, created_at and updated_at
-    attr_accessible :id, :created_at, :updated_at, :project_id, :step, :revision, :status
+    attr_accessible :project_id, :step, :revision, :status
+
+    # To not break the build reporter:
+    #
+    # These won't show up in the blacklist unless they're listed here. But
+    # they are somehow treated as blacklisted attributes anyway by ActiveRecord...
+    #
+    # If we don't blacklist them minimapper will try and set them which will
+    # lead to a ActiveModel::MassAssignmentSecurity::Error :(
+    attr_protected :created_at, :updated_at
   end
 end
