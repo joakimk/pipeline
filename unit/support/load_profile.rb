539011c18e8d8c8827f522ebd1cb07b098583493
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-12-26 20:02:51 +0100

    Fixing some issues with the profiling code.

--- unit/support/load_profile.rb
+++ unit/support/load_profile.rb
@@ -1,4 +1,7 @@
 if ENV['LOAD_PROFILE']
+  # Must be loaded before we use Time.now.
+  require 'timecop'
+
   puts "profiling enabled."
   module Kernel
     SLOW = (ENV['SLOW'] && ENV['SLOW'].to_f) || 0.05
@@ -6,13 +9,13 @@ if ENV['LOAD_PROFILE']
     [ :require, :load ].each do |method|
       alias_method "old_#{method}", method
 
-      define_method(method) do |file|
+      define_method(method) do |*opts|
         start_time = Time.now
-        ret = send("old_#{method}", file)
+        ret = send("old_#{method}", *opts)
         elapsed_time = Time.now - start_time
 
         if elapsed_time > SLOW
-          puts "Slow #{method}: #{file} #{(elapsed_time * 1000.0).to_i} ms"
+          puts "Slow #{method}: #{opts.first} #{(elapsed_time * 1000.0).to_i} ms"
         end
 
         ret
