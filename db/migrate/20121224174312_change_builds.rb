193315526126aecf65dd57db80f148c13d7cb7b6
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2013-02-25 22:31:24 +0100

    Fix CI build

--- db/migrate/20121224174312_change_builds.rb
+++ db/migrate/20121224174312_change_builds.rb
@@ -3,9 +3,9 @@ class ChangeBuilds < ActiveRecord::Migration
     rename_column :builds, :step, :step_name
     add_column :builds, :project_name, :string
 
-    AR::Build.reset_column_information
-    AR::Build.order('id ASC').each do |build|
-      build.update_attribute :project_name, AR::Project.find(build.project_id).name
+    BuildMapper::Record.reset_column_information
+    BuildMapper::Record.order('id ASC').each do |build|
+      build.update_attribute :project_name, ProjectMapper::Record.find(build.project_id).name
     end
 
     remove_column :builds, :project_id
