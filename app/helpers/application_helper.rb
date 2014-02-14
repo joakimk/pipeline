bc04ccd275ccc7619f94c08cdbba9a09919e90e1
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2014-02-14 22:57:48 +0100

    Cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def menu_item(name)
-    MenuItem.new(name, @active_menu_item_name)
+  def menu_item_class(name)
+    MenuItem.new(name, @active_menu_item_name).class
   end
 end
