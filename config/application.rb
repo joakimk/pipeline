ba8417e936f2a70b7a30e1a6a165e3e1638e46ea
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2014-02-15 13:56:43 +0100

    Styling revisions somewhat like what we have on our dev dashboard.

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ module Deployer
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/use_cases #{config.root}/app/repositories)
+    config.autoload_paths += %W(
+      #{config.root}/lib
+      #{config.root}/app/use_cases
+    )
 
     config.assets.initialize_on_precompile = false
 
