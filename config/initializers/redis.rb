30ee0f3478a39c0ecc01fc90a8a5f1109b76bd36
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-01-18 00:51:05 +0100

    Use different redis databases for dev and test.
    That way we can use the same server for both.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse(ENV["REDISCLOUD_URL"] || "redis://localhost:6379")
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, db: Rails.env.test? ? 1 : 0)
