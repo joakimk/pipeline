11847a245257d5b5f15b6acd153396c8e3d42e5d
Fabian Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-12-11 16:24:49 +0100

    @projects is now a local variable

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,25 +1,24 @@
 class ProjectsController < WebController
+  before_action :get_projects
+
   def index
-    @projects = Project.all_sorted
     revision_amount = 2
     locals revision_amount: revision_amount
   end
 
   def show
-    @projects = Project.all_sorted
-    @project = Project.find(params[:id])
+    project = Project.find(params[:id])
     revision_amount = 15
     locals :show,
-      revision_amount: revision_amount
+      {revision_amount: revision_amount,
+      project: project}
   end
 
   def edit
-    @projects = Project.all_sorted
     @project = Project.find(params[:id])
   end
 
   def update
-    @projects = Project.all_sorted
     project = Project.find(params[:id])
 
     if project.update_attributes(project_params)
@@ -44,5 +43,9 @@ class ProjectsController < WebController
 
   def setup_menu
     active_menu_item_name :projects
+  end
+
+  def get_projects
+    @projects = Project.all_sorted
   end
 end
