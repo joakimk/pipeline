abf86e1cfb2186ebe546e81cdc6e983871d460ae
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-12-11 16:39:48 +0100

    Temp: Debug prod errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Pipeline::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
